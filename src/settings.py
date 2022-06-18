2f26be12b0f54accd5d4599ac22a0043903f999b
myrage <hakeemmy056@gmail.com>
2022-06-18 12:22:59 +0100

    modified settings

--- src/settings.py
+++ src/settings.py
@@ -141,5 +141,5 @@ CORS_ALLOW_CREDENTIALS = True
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "hakeemy056@gmail.com"
-EMAIL_HOST_PASSWORD = "wxmymnrqbfyyyrvv"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
